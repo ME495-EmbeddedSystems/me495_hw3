7e5fff8ddb7a511234159c3cceb4385dd3cf58c4
Jarvis Schultz <schultzjarvis@gmail.com>
2015-12-01 18:13:02 -0600

    Improved deps and added tf.

--- src/measurement_server.py
+++ src/measurement_server.py
@@ -2,14 +2,20 @@
 import rospy
 from me495_hw3.srv import WallMeasurement
 from turtlesim.msg import Pose
+import tf
 import random
 
 yval = None
 NOISE = 0.1
 
-def posecb(data):
+def posecb(data, br):
     global yval
     yval = data.y
+    br.sendTransform((data.x, data.y, 0.0),
+                     tf.transformations.quaternion_from_euler(0,0,data.theta),
+                     rospy.Time.now(),
+                     "turtle_frame",
+                     "world")
     return
 
 
@@ -21,8 +27,9 @@ def measurement_servicecb(request):
 def main():
     global noise
     rospy.init_node("measurement_server", log_level=rospy.INFO)
+    br = tf.TransformBroadcaster()
     noise = rospy.get_param("~noise", NOISE)
-    rospy.Subscriber("pose", Pose, posecb)
+    rospy.Subscriber("pose", Pose, posecb, callback_args=br)
     meas_server = rospy.Service("measurement", WallMeasurement, measurement_servicecb)
     rospy.spin()
     
